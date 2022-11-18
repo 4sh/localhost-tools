7db69d9445440e45ca7f071c9284670cf21d44ac
Xavier Hanin <xavier.hanin@4sh.fr>
2022-11-19 00:43:48 +0100

    doc: add some doc

--- localhost-dev-proxy/dev-proxy-lib/src/main/java/sh/quatre/localhost/dev/proxy/api/LocalDevServer.java
+++ localhost-dev-proxy/dev-proxy-lib/src/main/java/sh/quatre/localhost/dev/proxy/api/LocalDevServer.java
@@ -7,6 +7,31 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+/**
+ * API of LocalDevServer localhost tool.
+ * <p>
+ * Use it in your app server to register your server against the localhost server manager, which will assign a free port
+ * to your app server.
+ * <p>
+ * It will also automatically unregister it on jvm close (or you can call `serverStopped` manually).
+ * <p>
+ * The server will then be able to be reached at http://[your-server-name].localtest.me:9999/
+ * <p>
+ * And it will show up in the list of servers at <a href="http://localhost:9999/">http://localhost:9999/</a>
+ * <p>
+ * Make sure a localhost server manager is running before launching your server.
+ * <p>
+ * Example of use with http4k:
+ * <code>
+ * fun main() {
+ * val handler = { request: Request -> Response(OK).body("Hello, I'm ${request.header("host")}!") }
+ * <p>
+ * val port = LocalDevServer.registerAndGetPort("my-http4k-server")
+ * logger.info("starting server on port $port")
+ * handler.asServer(Undertow(port)).start()
+ * }
+ * </code>
+ */
 public class LocalDevServer {
     private static final int localDevProxyControllerPort = 9990;
 
